0eebb016840eabc26e7b58e2af8059ac8626774d
Pritish Chakraborty <chakrabortypritish@gmail.com>
2020-04-10 20:03:57 +0530

    Section 19: Working with Redux Form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-	return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+	renderError = ({ error, touched }) => {
+		if (touched && error) {
+			return (
+				<div className="ui error message">
+					<div className="header">{error}</div>
+				</div>
+			);
+		}
+	}
+
+	renderInput = (formProps) => {
+		const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
+		// redux-form automates the implementation of forms with redux.
+		// It does not know anything about the way we're designing our app and how we're designing DOM elements.
+		// We specify onChange and value as usual, to create a controlled input.
+		// return <input onChange={formProps.input.onChange} value={formProps.input.value} />
+		// ES6 syntax that takes all the properties on the `input` object and assigns them as props to the input element.
+		return (
+			<div className={className}>
+				<label>{formProps.label}</label>
+				<input { ...formProps.input } />
+				{this.renderError(formProps.meta)}
+			</div>
+		);
+	}
+
+	// Generally submit callbacks take `event` as arg, but this is a redux-form situation.
+	onSubmit = (formValues) => {
+		// redux-form requires us to wrap the onSubmit callback with handleSubmit.
+		// redux-form handles the preventDefault for us, so we don't need this!
+		// event.preventDefault();
+		console.log(formValues);
+	}
+
+	render() {
+		return (
+			<form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+				{ /* label is not a redux-form prop, it is our custom prop */ }
+				<Field label="Enter title" name="title" component={this.renderInput}/>
+				<Field label="Enter description" name="description" component={this.renderInput}/>
+				<button className="ui button primary">Submit</button>
+			</form>
+		);
+	}
+}
+
+const validate = (formValues) => {
+	const errors = {};
+
+	// The errors object is passed down to the renderInput method.
+	if (!formValues.title) {
+		errors.title = "Enter a valid title";
+	}
+	if (!formValues.description) {
+		errors.description = "Enter a valid description";
+	}
+
+	return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+	form: 'streamCreate',
+	validate
+})(StreamCreate);
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+// Redux-form handles action creators, reducers and mapStateToProps related to forms for us.
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './AuthReducer';
 
 export default combineReducers({
-	auth: authReducer
+	auth: authReducer,
+	form: formReducer // redux-form requires us to attach the reducer to the key `form`.
 });
